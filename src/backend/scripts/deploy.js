f5f9d7271df0ed048908509d5a009b5b5ad0a090
PhuuQuiii <nguyenphuqui170304@gmail.com>
2025-02-24 03:09:50 +0700

    tải ipfs và openzeppelin/contracts

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -1,35 +1,35 @@
 async function main() {
 
-  const [deployer] = await ethers.getSigners();
+  const [deployer] = await ethers.getSigners(); // Lệnh này lấy danh sách các tài khoản có trong Hardhat ( tài khoản đầu tiên làm tài khoản triển khai hợp đồng )
 
-  console.log("Deploying contracts with the account:", deployer.address);
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log("Deploying contracts with the account:", deployer.address); // địa chỉ ví
+  console.log("Account balance:", (await deployer.getBalance()).toString()); //  số dư tài khoản
 
   // deploy contracts here:
   
   
   // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
-  saveFrontendFiles();
+  saveFrontendFiles(); // Lưu trữ địa chỉ và ABI của smart contract để frontend sử dụng.
 }
 
-function saveFrontendFiles(contract, name) {
+function saveFrontendFiles(contract, name) { // Lưu trữ thông tin hợp đồng
   const fs = require("fs");
-  const contractsDir = __dirname + "/../../frontend/contractsData";
+  const contractsDir = __dirname + "/../../frontend/contractsData"; // thư mục chứa thông tin hợp đồng trong frontend.
 
   if (!fs.existsSync(contractsDir)) {
     fs.mkdirSync(contractsDir);
   }
 
   fs.writeFileSync(
-    contractsDir + `/${name}-address.json`,
+    contractsDir + `/${name}-address.json`, // Lưu địa chỉ hợp đồng vào file JSON
     JSON.stringify({ address: contract.address }, undefined, 2)
   );
 
   const contractArtifact = artifacts.readArtifactSync(name);
 
   fs.writeFileSync(
-    contractsDir + `/${name}.json`,
-    JSON.stringify(contractArtifact, null, 2)
+    contractsDir + `/${name}.json`, // Lấy ABI của hợp đồng và lưu vào file JSON.
+    JSON.stringify(contractArtifact, null, 2) 
   );
 }
 
