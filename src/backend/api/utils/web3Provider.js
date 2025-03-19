44ba869b900542abd53fd6476bed4240ff7e9d02
PhuuQuiii <nguyenphuqui170304@gmail.com>
2025-03-19 14:20:44 +0700

    set biến tĩnh thay biến môi trường

--- src/backend/api/utils/web3Provider.js
+++ src/backend/api/utils/web3Provider.js
@@ -2,7 +2,7 @@ const { ethers } = require("ethers");
 require("dotenv").config();
 
 // Kết nối tới blockchain thông qua Ganache hoặc mạng tùy chỉnh
-const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545");
 
 async function getSigner() {
   const accounts = await provider.listAccounts();
